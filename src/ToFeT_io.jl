52badfbbdfed679f4dae7d7d2badca01d74eddc1
Jarvist Moore Frost <jarvist@gmail.com>
2020-05-24 13:41:26 +0100

    generate sparse (master equation) rate matrix, with diag

--- src/ToFeT_io.jl
+++ src/ToFeT_io.jl
@@ -5,15 +5,28 @@ import DelimitedFiles
 import SparseArrays
 
 function readEdges(f)
-    COO=DelimitedFiles.readdlm(f)
+    raw=DelimitedFiles.readdlm(f)
     
-    # renumber indices from 1 rather than zero
-    As=Int.(vcat(COO[:,1],COO[:,2])) .+ 1
-    Bs=Int.(vcat(COO[:,2],COO[:,2])) .+ 1
+    # - .+1 renumber indices from 1 rather than zero
+    # - double up (vcat) indices for making explicit A->B && implicit B->A
+    As=Int.(vcat(raw[:,1],raw[:,2])) .+ 1
+    Bs=Int.(vcat(raw[:,2],raw[:,2])) .+ 1
 
-    Js=vcat(COO[:,3],COO[:,3])
+    Js=vcat(raw[:,3],raw[:,3])
+    
+    SparseArrays.sparse(As,Bs,Js)
+end
+
+function ratematrix(edges)
+    # translate back into the dense CSC repr; 
+    # saves much time and only generates entries for nonzeros
+    As,Bs,Js=SparseArrays.findnz(edges)
+    
     rates=exp.(-Js)
-
-    ratematrix=SparseArrays.sparse(As,Bs,rates)
+    
+    dim=edges.m # number of sites
+    diag=dropdims( - sum(SparseArrays.sparse(As,Bs,rates), dims=2), dims=2 )
+    
+    SparseArrays.sparse(vcat(As,1:dim),vcat(Bs,1:dim),vcat(rates,diag) )
 end
 
