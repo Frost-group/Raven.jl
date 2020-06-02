912bcb548927ccc56891ac8d28c356861e5a1848
Jarvist Moore Frost <jarvist@gmail.com>
2020-06-02 21:18:18 +0100

    Yu's iterative / power method for the non-linear master equation.

--- src/rates.jl
+++ src/rates.jl
@@ -26,6 +26,31 @@ function ratematrix(edges,ratefn)
     SparseArrays.sparse(vcat(As,1:dim),vcat(Bs,1:dim),vcat(rates,diag) )
 end
 
+function bareratematrix(edges,ratefn)
+    # translate back into the dense COO repr; 
+    # saves much time and only generates entries for nonzeros
+    As,Bs,Js=SparseArrays.findnz(edges)
+    
+    rates=ratefn.(Js) # this applies for each element
+    
+    SparseArrays.sparse(As, Bs, rates )
+end
+
+"Single iteration of Yu's power iterative method to solve master equation. Needs a bare ratematrix. (i.e. zeros / empty on the diagonal).
+
+See IV.C. (equation 20):
+Yu, Z. G., Smith, D. L., Saxena, A., Martin, R. L., Bishop, A. R. (2001). 
+Molecular geometry fluctuations and field-dependent mobility in conjugated polymers. 
+Physical Review B, 63(8), 085202. https://doi.org/10.1103/PhysRevB.63.085202
+"
+function YuPRBpowm!(ω, P)
+    Σ=sum # just for notation niceness
+    for i in eachindex(P)
+        P[i] = Σ(ω[i,:] .* P[:]) / Σ(ω[:,i]) / (1 - Σ( (ω[:,i].-ω[i,:]) .* P[:]) / Σ(ω[:,i]) ) 
+    end
+    P
+end
+
 # None of the below actually works / is tested. Here be dragons.
 
 function solveMasterDense(rates)
