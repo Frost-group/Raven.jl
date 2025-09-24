b9146581da0cdb08d275aeefb4ae4d827890f614
Kuno <rk820@ic.ac.uk>
2025-09-24 13:59:43 +0100

    data structure for lattice and ions corrected: lattice a 3D array and ion list 4D array where [id, x, y, z]

--- src/mc.jl
+++ src/mc.jl
@@ -1,19 +1,26 @@
 import LinearAlgebra, IterativeSolvers
 
-function initializesystem(a,b,c,O) # creating a physical system of occupancy(0s and 1s, expand later to +1s and -1s). x,y,z for dimensions and O for the occupancy fraction.
-    N = round(a * b * c * O)
-    A = [Vector{Int}() for _ in 1:a, _ in 1:b, _ in 1:c]
-    ionlist=[]
+function initializesystem(a,b,c,f) # creating a physical system of occupancy(0s and 1s, expand later to +1s and -1s). x,y,z for dimensions and O for the occupancy fraction.
+    N = round(a * b * c * f)
+    N = Int64(N)
+    lattice = zeros(Int64, a, b, c)
+    ionlist = zeros(Int64, N, 4)
     id=1
-    placed=0
-    while placed < N
+    while id - 1 < N
         x, y, z = rand(1:a), rand(1:b), rand(1:c)
-        if isempty(A[x, y, z])
-            push!(A[x, y, z], id)
-            push!(ionlist, id)
-            id = id + 1
-            placed = placed + 1
-        end
+        lattice[x, y, z] = id 
+        ionlist[id, :] = [id, x, y, z]
+        id = id + 1
     end
-    return A, ionlist
+    return lattice, ionlist
 end
+
+"""
+function checkneighbors(A) # selecting a random site from the ion list and checking if its neighbors are occupied or not.
+    id = rand(ionlist)
+    I = findfirst(cell -> (id in cell, A))
+    neighbors = []
+    push![(I[1] ± 1, I[2], I[3]), neighbors]
+    push![(I[1], I[2] ± 1, I[3]), neighbors]
+    push![(I[1], I[2], I[3] ± 1), neighbors]
+"""
