7d356c4b84ce600f410ed7b017df5a2b75a1b261
Kuno <rk820@ic.ac.uk>
2025-10-29 17:26:19 +0000

    got rid of flattening/unflattening. rough idea is there, need to fix tracking dr.

--- src/mc.jl
+++ src/mc.jl
@@ -2,73 +2,68 @@ import LinearAlgebra, IterativeSolvers, Random
 
 # Version 2 initialization code based on flattened data.
 
-function initialize(a, b, c, f)
-    S = a * b * c # S is the already 'flattened' 1D index that can be linked 1 to 1 to the original 3D lattice via the 'unflatten' function.
-    N = round(a * b * c * f) # number of ions to be generated within the flattened lattice.
-    N = Int(N)
-    #d0 = fill((0.0, 0.0, 0.0), N) # initialization of positions of each ion per row. later used for MSD computation.
-    dr = fill((0.0, 0.0, 0.0), N) # relative position tracker per sweep, also later used for MSD computation.
-    sel = Random.randperm(S)[1:N] # shuffle flattened matrix S to select the first N entries to determine where ions reside in.
-    pos = Vector{Int}(undef, N)
-    occ = fill(0, S)
+function initialize(a, b, c, N)
+    S = a * b * c # lattice size
+    pos = Matrix{Int}(undef, N, 3) # N rows, columns = (x, y, z)
+    occ = fill(0, a, b, c)
+    disp = fill(0, N, 3) # relative position tracker per sweep, also later used for MSD computation.
+    picks = Random.randperm(S)[1:N] # shuffle matrix S to select the first N entries to determine where ions reside in.
+    CI = CartesianIndices((a, b, c))
     for id in 1:N
-        pos[id] = sel[id]
-        occ[sel[id]] = id
+        x, y, z = Tuple(CI[picks[id]])
+        pos[id, :] .= (x, y, z)
+        occ[x, y, z] = id
     end
-    return a, b, c, pos, occ, dr
-end   
-
-function flatten(a, b, c, x, y, z) # flattening 3D coordinates to a linear index coordinate.
-    LinearIndices((a, b, c))[x, y, z]
-end
-
-function unflatten(s, a, b, c)
-    Tuple(CartesianIndices((a, b, c))[s])
+    return a, b, c, pos, occ, disp
 end
 
 function neighbors(a, b, c, pos, id) 
 # Note that this function doesn't return the neighbors of, for example, site 7 but the ION with the ID #7!
-    target = pos[id]
-    nbrs = Vector{Int}(undef, 6)
-    (x, y, z) = unflatten(target, a, b, c)
+    x = pos[id, 1]; y = pos[id, 2]; z = pos[id, 3]
+    nbrs = Matrix{Int}(undef, 6, 3)
     k = 1
     for (dx, dy, dz) in ((1, 0, 0), (-1, 0, 0), (0, 1, 0), (0, -1, 0), (0, 0, 1), (0, 0, -1))
         nx = mod1(x + dx, a)
         ny = mod1(y + dy, b)
         nz = mod1(z + dz, c)
-        nbrs[k] = flatten(a, b, c, nx, ny, nz)
+        nbrs[k, :] .= (nx, ny, nz)
         k += 1
     end
     return nbrs
 end
 
-function mcstep!(a, b, c, pos, occ, dr)
-    stepid = rand(1:length(pos))
-    oldpos = pos[stepid]
-    nbrs = neighbors(a, b, c, pos, stepid)
-    newpos = rand(nbrs)
-    if occ[newpos] == 0 # check if selected neighbor is empty
-        occ[oldpos] = 0 # empty the old position
-        occ[newpos] = stepid # put the ion in the new position
-        pos[stepid] = newpos # update the #ID entry in the ion list
+function mcstep!(a, b, c, pos, occ, disp; verbose=false)
+    ion = rand(1:length(pos[:, 1]))
+    oldpos = pos[ion, :]
+    nbrs = neighbors(a, b, c, pos, ion)
+    newpos = nbrs[rand(1:6), :]
+    if occ[newpos[1, 1], newpos[2, 1], newpos[3, 1]] == 0 # check if selected neighbor is empty
+        occ[oldpos[1, 1], oldpos[2, 1], oldpos[3, 1]] = 0 # empty the old position
+        occ[newpos[1, 1], newpos[2, 1], newpos[3, 1]] = ion # put the ion in the new position
+        pos[ion, :] = newpos # update the #ID entry in the ion list
 
         # load old and new lattice positions
-        x, y, z = unflatten(oldpos, a, b, c)
-        nx, ny, nz = unflatten(newpos, a, b, c)
+        x, y, z = oldpos[1, 1], oldpos[2, 1], oldpos[3, 1]
+        nx, ny, nz = newpos[1, 1], newpos[2, 1], newpos[3, 1]
 
         # raw differences before PBC correction
         dx = nx - x
         dy = ny - y
         dz = nz - z
 
-        # PBC handling: when crossing the +x boundary, a -> 1 should be +1 and not 1-a
-        if dx == (a - 1); dx = -1 elseif dx == 1 - a; dx = 1 end
-        if dy == (b - 1); dy = -1 elseif dy == 1 - b; dy = 1 end
-        if dz == (c - 1); dz = -1 elseif dz == 1 - c; dz = 1 end
-        
+        # Directly asking whether we stayed on the same coordinate, the next cell, or the previous one?
+        dx = (nx == x) ? 0 : (nx == mod1(x + 1, a) ? 1 : -1)
+        dy = (ny == y) ? 0 : (ny == mod1(y + 1, b) ? 1 : -1)
+        dz = (nz == z) ? 0 : (nz == mod1(z + 1, c) ? 1 : -1)
+
         # adding each displacement to dr
-        drx, dry, drz = dr[stepid]
-        dr[stepid] = (drx + dx, dry + dy, drz + dz)
+        disp[ion, 1] += dx
+        disp[ion, 2] += dy
+        disp[ion, 3] += dz
+
+        if verbose
+            println("dx:$dx dy:$dy dz:$dz")
+        end
 
         return true
     else
@@ -76,22 +71,24 @@ function mcstep!(a, b, c, pos, occ, dr)
     end
 end
 
-function mcloop!(a, b, c, f, steps)
-    a, b, c, pos, occ, dr = initialize(a, b, c, f)
-    attempts = length(dr)
-    sweeps = round(steps/attempts)
+function mcloop!(a, b, c, N, steps)
+    a, b, c, pos, occ, disp = initialize(a, b, c, N)
+    attempts = N
+    sweeps = cld(steps, attempts)
+    dr_log = Array{Int}(undef, sweeps, N, 3)
     for sweep in 1:sweeps
         for attempt in 1:attempts
-            mcstep!(a, b, c, pos, occ, dr)
-            i = attempt + attempts * (sweep - 1)
-            print("step $i is complete. \n")
+            mcstep!(a, b, c, pos, occ, disp, verbose=true)
+            println("attempt $(attempt + attempts * (sweep - 1))")
         end
-        disp = dr
-        dr = append!(disp, dr)
+        dr_log[sweep, :, :] .= disp
+        println("sweep $sweep / $sweeps")
     end
-    return dr
+    return dr_log, pos, occ
 end
 
+
+# we might need to get rid of the flattening afterall. RIP.
 """
 function flatten(X, Y, Z, x, y, z) # for flattening 3D coordinates to indexing i.
     return x + yX + zXY
@@ -123,6 +120,14 @@ function listneighbors(id, ionlist)
                0  0 -1 ] # sooo much cleverer using a delta -- gotta thank Jarv!
     neighbors = target .+ deltas
     return neighbors
+end
+
+function flatten(a, b, c, x, y, z) # flattening 3D coordinates to a linear index coordinate.
+    LinearIndices((a, b, c))[x, y, z]
+end
+
+function unflatten(s, a, b, c)
+    Tuple(CartesianIndices((a, b, c))[s])
 end
 """
 # Version 1 demo before implementing data flattening below.
